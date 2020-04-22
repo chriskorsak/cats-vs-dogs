ba41be4747474933e691fabe5790673479efcd9c
chriskorsak <chris@chriskorsak.com>
2020-04-22 09:13:36 -0700

    fix jquery code so blank list element does not appear when clicking buttons. add 3rd button two demonstrate jquery .after method.

--- js/scripts.js
+++ js/scripts.js
@@ -1,11 +1,22 @@
 $(document).ready(function() {
+
   $("button#cat").click(function() {
-    $("ul#cat-says").prepend("<li>MEOW!<li>");
-    $("ul#dog-says").prepend("<li>BARK!<li>");
+    $("ul#cat-said").prepend("<li>meow</li>");
+    $("ul#dog-said").prepend("<li>Is that all you got?</li>");
+    $("ul#cat-said").children("li").first().click(function() {
+      $(this).remove();
+    });
   });
   $("button#dog").click(function() {
-    $("ul#cat-says").prepend("<li>OH NO!<li>");
-    $("ul#dog-says").prepend("<li>BARK, BARK!<li>");
+    $("ul#cat-said").prepend("<li>OH NO!</li>");
+    $("ul#dog-said").prepend("<li>BARK, BARK!</li>");
+  });
+  $("button#after-h1").click(function() {
+    $("h1").after("<p>This paragraph is after the h1 element!</p>");
+    $("p").click(function() {
+      (this).remove();
+    });
+
   });
 
 });
